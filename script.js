e27732ef0e5e6fe2f8506e3b0c14dc5e435f2e8c
Moazzam Moriani <mozmoriani@gmail.com>
2021-07-10 22:29:04 +0500

    Created An Ineractable GUI For the Game In order to create a GUI that works within the browser some code had to be refactored and some procedures has to be removed. Procedures that update the GUI has to be added like updateDisplay. The game could no longer rely on a while loop because then the web page started crashing.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-
+let playerScore = 0;
+let computerScore = 0;
+let totalGames= 0;
+const rockButton = document.getElementsByClassName("rock-optn")[0];
+const paperButton = document.getElementsByClassName("paper-optn")[0];
+const scissorsButton = document.getElementsByClassName("scissors-optn")[0];
+const roundResults = document.getElementsByClassName("round-results")[0];
+const playerRoundInfo = document.getElementsByClassName("player-round-info")[0];
+const computerRoundInfo = document.getElementsByClassName("computer-round-info")[0];
+const playerSelectionDisplay = document.getElementsByClassName("player-selection")[0];
+const computerSelectionDisplay = document.getElementsByClassName("computer-selection")[0];
+rockButton.onclick =  function() {gameLog(0)};
+paperButton.onclick = function() {gameLog(1)};
+scissorsButton.onclick = function() {gameLog(2)};
 
 // Computer randomly picks an number 0, 1, or 2 which will eventually be mapped
 // to rock, paper or scissors, respectively.
@@ -8,39 +21,62 @@ function computerSelection() {
 
 // Get string input from from user and convert it to a number for the game's 
 // model.
-function playerSelection() {
-    let playerChoice = prompt("Rock, Paper, or Scissors?");
-    playerChoice = playerChoice.toLowerCase();
-    return mapChoiceToNum(playerChoice);
-}
 
-//Mapping rock, paper and scissors to 0, 1 and 2 respectively 
-function mapChoiceToNum(choice) {
-    switch(choice) {
-        case "rock":
-            return 0;
-        case "paper":
-            return 1;
-        case "scissors":
-            return 2;
-    }
-}
-// Takes in a number representations of rock, paper, and scissors as input from
+
+
 // two players and returns the result of the round.
 function playRound(playerOne, playerTwo) {
 let choices = ["rock", "paper", "scissors"];
 
     if (playerOne == playerTwo) {
-        return `Both chose ${choices[playerOne]}. It's a tie!`
+        return {tie: true, winner: null, playerChoice: choices[playerOne], computerChoice: choices[playerTwo]}
     } else if ((playerOne + 1) % 3 == playerTwo ) {
-        return `You lost. You chose ${choices[playerOne]} and the Computer chose ${choices[playerTwo]}`
+        return {tie: false, winner: "computer", playerChoice: choices[playerOne], computerChoice: choices[playerTwo]}
     } else {
-        return `You won the round! You chose ${choices[playerOne]} and the Computer chose ${choices[playerTwo]}`
+        return {tie: false, winner: "player", playerChoice: choices[playerOne], computerChoice: choices[playerTwo]}
     }
 }
 
-function game() {
-    alert(playRound(playerSelection(), computerSelection()));
+function updateDisplay(displayNode, newDisplayItem) {
+    let itemSource = "images/" + newDisplayItem + ".png"
+    if (displayNode.childElementCount !== 0 ) {
+        displayNode.removeChild(displayNode.children[0]);
+    } 
+    let img = document.createElement("img")
+    img.setAttribute("src", itemSource);
+    displayNode.appendChild(img);
+    
+}
+
+
+function gameLog(playerChoice) {
+    let round = playRound(playerChoice, computerSelection());
+    updateDisplay(playerSelectionDisplay, round.playerChoice);
+    updateDisplay(computerSelectionDisplay, round.computerChoice);
+
+    totalGames += 1;
+
+    if (!round.tie) {
+        if (round.winner == "player") {
+            playerScore += 1;
+                
+        } else {
+            computerScore += 1;
+        }
+    } 
+
+    roundResults.innerHTML = `Round - ${totalGames}`;
+
+    playerRoundInfo.innerText = `You - ${playerScore}`;
+    computerRoundInfo.innerText = `Computer - ${computerScore}`;
+
+    if (playerScore >= 5) {
+        roundResults.innerHTML = "You won!";
+        alert("Press F5 to play again");
+    } else if (computerScore >= 5) {
+        roundResults.innerHTML = "You lost";
+        alert("Press F5 to play again");
+    }
 }
 
 
